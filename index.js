b4758d6804d0682b6cf9c8191d4f33c2f32ed6ed
Hmonsur55 <116294687+Hmonsur55@users.noreply.github.com>
2023-05-13 23:27:07 +0600

    get checkout data in checkout url

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000
@@ -29,14 +29,26 @@ async function run() {
       await client.connect();
       
       const dataCollection = client.db("carDoctor").collection("services")
-      
+  
+      //   to get all data
+
       app.get('/services', async(req, res) => {
           const cursor = dataCollection.find()
           const result =  await cursor.toArray()
           res.send(result);
       })
-
-
+//  to get single data by id 
+      
+      app.get('/checkout/:id', async(req, res) => {
+          const id = req.params.id;
+          const query = { _id: new ObjectId(id) }
+           const options = {
+          projection: { title: 1, price: 1 },
+        };
+          const result = await dataCollection.findOne(query, options)
+          res.send(result);
+      })
+      
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
