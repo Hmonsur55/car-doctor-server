1fcaaa98687c18f0f3249af1f1273e29e9f48301
Hmonsur55 <116294687+Hmonsur55@users.noreply.github.com>
2023-05-15 09:51:36 +0600

    add delete method

--- index.js
+++ index.js
@@ -69,7 +69,15 @@ async function run() {
       const result = await bookCollection.insertOne(booking);
       res.send(result);
     })
-      
+
+     app.delete('/bookings/:id', async (req, res) => {
+       const id = req.params.id
+       console.log(id)
+      const query = { _id: new ObjectId(id) }
+      const result = await bookCollection.deleteOne(query)
+      res.send(result)
+     })
+     
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
